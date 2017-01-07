180735376a4db904c69e7d38d5d97251248cc8d3
unknown <nadjam@336-B7.nad.qc.ca>
2017-01-07 06:13:07 -0500

    net base updated, works with matchmaker

---
+++ Assets/Network/NetActionnable.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class NetActionnable : NetworkBehaviour {
+
+	[SyncVar]
+	bool effectState;
+
+	// Use this for initialization
+	void Start () {
+		effectState = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		//if (this.gameObject.GetComponent<NetworkIdentity> ().isServer) {
+			Debug.Log ("action trigger!" + effectState);
+			effectState = !effectState;
+		//} else {
+			
+		//}
+
+	}
+}
---
+++ Assets/Network/NetworkCalls.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class NetworkCalls : NetworkBehaviour {
+
+	NetworkMatch matchMaker;
+
+	public Canvas canvas;
+
+	private MatchInfoSnapshot currentMatch;
+
+	// Use this for initialization
+	void Start () {
+		NetworkManager.singleton.StartMatchMaker ();
+		matchMaker = NetworkManager.singleton.matchMaker;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartMatch(){
+		matchMaker.CreateMatch ("room", 2, true, "", "", "", 0, 1, NetworkManager.singleton.OnMatchCreate);
+		canvas.enabled = false;
+	}
+
+	public void JoinMatch(){
+		matchMaker.ListMatches (0, 2, "", false, 0, 1, JoinActualMatch);
+		canvas.enabled = false;
+	}
+
+	void JoinActualMatch(bool success, string info, List<MatchInfoSnapshot> matches) {
+		Debug.Log (matches);
+		currentMatch = matches.FirstOrDefault ();
+		matchMaker.JoinMatch (currentMatch.networkId, "", "", "", 0, 1, NetworkManager.singleton.OnMatchJoined);
+	}
+
+	public void DestroyMatch(){
+		matchMaker.DestroyMatch (currentMatch.networkId, 1, NetworkManager.singleton.OnDestroyMatch);
+	}
+
+}
