2ce0d2d2fb73de451a70ac1c7ef611a777598c94
unknown <nadjam@336-B7.nad.qc.ca>
2017-01-07 02:14:55 -0500

    detect fire1

--- Assets/Scripts/NetMove.cs
+++ Assets/Scripts/NetMove.cs
@@ -3,13 +3,16 @@ using UnityEngine.Networking;
 using System.Collections;
 
 public class NetMove : MonoBehaviour {
+	public GameObject ActionZone;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
+	/// <summary>
+	/// Update this instance.
+	/// </summary>
 	void Update () {
 
 		if (!this.gameObject.GetComponent<NetworkIdentity>().isLocalPlayer) return;
@@ -18,5 +21,16 @@ public class NetMove : MonoBehaviour {
 		var z = Input.GetAxis("Vertical")*0.1f;
 
 		transform.Translate(x, 0, z);
+
+		if (Input.GetButton ("Fire1")) {
+			ActivateEffect ();
+		}
+	}
+
+	/// <summary>
+	/// Activates the effect.
+	/// </summary>
+	private void ActivateEffect (){
+		Debug.Log ("effect~"+ this.gameObject.GetComponent<NetworkIdentity>().isServer);
 	}
 }
