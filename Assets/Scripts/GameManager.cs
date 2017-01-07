e43f2d7df53acf6795279e9cf99df9532d5b06ad
unknown <nadjam@336-B4.nad.qc.ca>
2017-01-07 04:33:47 -0500

    added logic for next level loading

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    static public GameManager instance = null;
+    public string[] Levels;
+    public int CurrentLevel;
+
+    // Use this for initialization
+    void Start() {
+        // singleton logic
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void StartNewGame()
+    {
+        // Level 0 is menu
+        CurrentLevel = 0;
+        LoadNextLevel();
+    }
+
+    public void LoadNextLevel()
+    {
+        if (Levels.Length < 0) return;
+
+        // Level 0 is menu
+        CurrentLevel++;
+        Debug.Log("HEADING TO NEXT LEVEL " + CurrentLevel);
+        CurrentLevel = CurrentLevel%Levels.Length;
+        SceneManager.LoadScene(Levels[CurrentLevel]);
+    }
+}
