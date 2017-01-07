952f5fe3b69cf6c2bdcb511f922af5147318ab39
unknown <nadjam@336-B7.nad.qc.ca>
2017-01-07 01:53:32 -0500

    net test

---
+++ Assets/Scripts/NetMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class NetMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!this.gameObject.GetComponent<NetworkIdentity>().isLocalPlayer) return;
+		
+		var x = Input.GetAxis("Horizontal")*0.1f;
+		var z = Input.GetAxis("Vertical")*0.1f;
+
+		transform.Translate(x, 0, z);
+	}
+}
