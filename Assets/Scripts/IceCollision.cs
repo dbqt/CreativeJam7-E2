ac26420557853f23993f3c4bd07a44f72de105b9
unknown <nadjam@336-B4.nad.qc.ca>
2017-01-07 23:54:09 -0500

    added Frost prefab

---
+++ Assets/Scripts/IceCollision.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceCollision : MonoBehaviour {
+    public void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<Actionnable>().IceAction();
+    }
+}
