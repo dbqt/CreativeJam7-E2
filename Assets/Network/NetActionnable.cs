80ca1b613e15340d000d39ede9f958cc4c0832d6
unknown <nadjam@336-B7.nad.qc.ca>
2017-01-08 00:26:24 -0500

    integrate peters models

--- Assets/Network/NetActionnable.cs
+++ Assets/Network/NetActionnable.cs
@@ -5,25 +5,14 @@ using System.Collections;
 public class NetActionnable : NetworkBehaviour {
 
 	[SyncVar]
-	bool effectState;
+	public bool effectState;
 
 	// Use this for initialization
 	void Start () {
 		effectState = false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
-	void OnTriggerEnter(Collider other) {
-		//if (this.gameObject.GetComponent<NetworkIdentity> ().isServer) {
-			Debug.Log ("action trigger!" + effectState);
-			effectState = !effectState;
-		//} else {
-			
-		//}
-
+	public virtual void ToggleState(){
+		effectState = !effectState;
 	}
 }
