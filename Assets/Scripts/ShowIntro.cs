31ab0ea1e1ea905895ed162d4fa090b08ccafa71
unknown <nadjam@336-B2.nad.qc.ca>
2017-01-08 14:22:21 -0500

    added logic for intro scene

---
+++ Assets/Scripts/ShowIntro.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class ShowIntro : MonoBehaviour {
+    public GameObject scene1, scene2, scene3;
+    public Animator anim;
+    int state = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void showNext() {
+        Debug.Log("Showing Next");
+        switch (state)
+        {
+            case 0:
+                anim.SetTrigger("showScene2");
+                break;
+            case 1:
+                anim.SetTrigger("showScene3");
+                break;
+            case 2:
+                GameManager.instance.LoadNextLevel();
+                break;
+        }
+        state++;
+    }
+}
