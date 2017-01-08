fe3300215a570aabf5d1b9dfe06aa326afd9e410
unknown <nadjam@336-B7.nad.qc.ca>
2017-01-08 03:21:45 -0500

    script is mine

---
+++ Assets/Scripts/IsMine.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsMine : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (GameManager.instance.isPlayerOne)
+			this.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
