39b96bdadf7f92e2ec2feca5a54a79cce798664b
unknown <nadjam@336-B1.nad.qc.ca>
2017-01-08 16:49:48 -0500

    added open door and water balance sounds

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -2,8 +2,17 @@
 using System.Collections;
 
 public class OpenDoor : MonoBehaviour {
+    AudioSource soundS;
+    public AudioClip open;
+
+    void Start()
+    {
+        this.soundS = (gameObject.AddComponent<AudioSource>() as AudioSource);
+        soundS.clip = open;
+    }
 
 	void OnTriggerEnter(Collider other) {
+        soundS.Play();
 		this.gameObject.GetComponent<Animator> ().SetBool ("isDoorOpen", true);
 	}
 
