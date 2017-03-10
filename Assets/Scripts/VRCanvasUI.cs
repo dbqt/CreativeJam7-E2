2410ac035890346095da763c9f052341bb6ff399
Pierre To <pierre.to@polymtl.ca>
2017-03-10 16:38:39 -0500

    Ajout d'un message VR

---
+++ Assets/Scripts/VRCanvasUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRCanvasUI : MonoBehaviour {
+
+	public bool showUI = true;
+	public GameObject ui;
+
+	// Use this for initialization
+	void Start () {
+		Invoke("HideUI", 4f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!showUI) {
+			ui.SetActive (false);
+		}
+	}
+
+	void HideUI() {
+		showUI = false;
+	}
+}
